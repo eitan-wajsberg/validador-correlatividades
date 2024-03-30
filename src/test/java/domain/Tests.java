7e75b550dfaf6154d31ed9b6f3af1ef8b5927b00
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-03-30 18:56:00 -0300

    Implemento los tests

--- src/test/java/domain/Tests.java
+++ src/test/java/domain/Tests.java
@@ -4,47 +4,51 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AlumnoTest {
+public class Tests {
     Materia ayed = new Materia("Algoritmos y Estructuras de Datos");
     Materia md = new Materia("Matematica Discreta");
     Materia pdp = new Materia("Paradigmas de Programacion");
     Materia syo = new Materia("Sistemas y Organizaciones");
     Materia ads = new Materia("Analisis de Sistemas");
-    Materia dds = new Materia("Diseño de sistemas");
+    Materia dds = new Materia("Diseño de Sistemas");
+    Materia ssl = new Materia("Sintaxis y Semantica de los Lenguajes");
     Alumno martin = new Alumno("Martin Martinez", "208.124.0");
+    Inscripcion i = new Inscripcion(martin);
 
     @Before
     public void setup() {
         pdp.agregarCorrelativas(ayed, md);
+        ssl.agregarCorrelativas(ayed, md);
         ads.agregarCorrelativas(syo, ayed);
         dds.agregarCorrelativas(ads, pdp);
+        martin.agregarMateriasAprobadas(ayed, md);
     }
 
     @Test
     public void alumnoPuedeCursarMateriaCuyasCorrelativasTieneAprobadas() {
-        martin.agregarMateriasAprobadas(ayed, md);
         Assert.assertTrue(martin.puedeCursarMateria(pdp));
     }
 
     @Test
     public void alumnoNoPuedeCursarMateriaQueYaTieneAprobada() {
-        martin.agregarMateriasAprobadas(ayed);
         Assert.assertFalse(martin.puedeCursarMateria(ayed));
     }
 
     @Test
     public void alumnoNoPuedeCursarMateriaCuyasCorrelativasNoTieneAprobadas() {
-        martin.agregarMateriasAprobadas(ayed);
-        Assert.assertFalse(martin.puedeCursarMateria(pdp));
+        Assert.assertFalse(martin.puedeCursarMateria(dds));
     }
 
     @Test
     public void inscripcionNoValidaSiElAlumnoNoPuedeCursarTodasLasMaterias() {
-        //TODO
+        i.agregarMaterias(dds, ads, ssl);
+        Assert.assertFalse(i.aprobada());
     }
 
     @Test
     public void inscripcionValidaSiElAlumnoPuedeCursarTodasLasMaterias() {
-        //TODO
+        martin.agregarMateriasAprobadas(ayed, md, pdp, ads);
+        i.agregarMaterias(dds, ssl);
+        Assert.assertTrue(i.aprobada());
     }
 }
